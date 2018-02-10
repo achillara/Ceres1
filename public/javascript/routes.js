e7a5e0b0b7bd8e23021f970cd3a12f6d1151c613
Anu <srianu98@yahoo.com>
2018-02-10 00:36:38 -0500

    so far so good

--- public/javascript/routes.js
+++ public/javascript/routes.js
@@ -1,11 +1,27 @@
 
 const request = require("request");
-const url = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=47.6044,-122.3345;47.6731,-122.1185;47.6149,-122.1936&destinations=45.5347,-122.6231;47.4747,-122.2057&travelMode=driving&key=Aig6lADJLSD9gGYuKIF9FUKB-AeXsb0Gc9KG4h8B1aF5_e10JGOv4a1OQYe5rYl4"
+var zipcodes = require('zipcodes');
+
+var l = zipcodes.lookupByName('Cupertino', 'CA');
+var lat1 = l[0].latitude;
+var long1 = l[0].longitude;
+
+var a = zipcodes.lookupByName('Cupertino', 'CA');
+var lat2 = a[0].latitude;
+var long2 = a[0].longitude;
+
+var t = zipcodes.lookupByName('Cupertino', 'CA');
+var lat3 = t[0].latitude;
+var long3 = t[0].longitude;
+
+const url = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=40.440495,-79.943945&destinations=" +lat1+","+long1+";"+lat2+","+long2+";"+lat3+","+long3+"&travelMode=driving&key=Aig6lADJLSD9gGYuKIF9FUKB-AeXsb0Gc9KG4h8B1aF5_e10JGOv4a1OQYe5rYl4"
+
+
 
 
 request.get(url, (error, response, body) => {
   let json = JSON.parse(body);
-  console.log(
-   	json.resourceSets[0];
-  );
+  console.log(json.resourceSets[0].resources[0].results[0].travelDuration)
+  console.log(json.resourceSets[0].resources[0].results[1].travelDuration)
+  console.log(json.resourceSets[0].resources[0].results[2].travelDuration);
 }); 
