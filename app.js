bac0000d4a3f053c3ad2bd97fae1c1bb3c005955
Anu <srianu98@yahoo.com>
2018-02-10 16:47:20 -0500

    changed form logistics

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 var express = require('express'); 
 var app = express(); 
 var server = require('http').createServer(app); 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 server.listen(port);
 
 app.use(express.static(__dirname + '/public'));
+app.get('/results', function(req, res){
+  //if html file is root directory
+ res.sendFile(__dirname + '/public/results.html'); //if html file is within public directory
+});
 
 
